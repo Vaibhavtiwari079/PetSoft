6c9e2ffba35fedef2f2c3dde2f1fed8b373007a3
Vaibhavtiwari079 <vaibhavtiwari.0504@gmail.com>
2024-07-24 10:49:01 +0530

    stripe js setup and aftereffects

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -2,16 +2,43 @@
 import { createCheckoutSession } from '@/actions/action'
 import H1 from '@/components/H1'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useTransition } from 'react'
 
-export function Page() {
+export function Page({searchParams}:{
+    searchP arams:{[key:string]:string | string[] |undefined}
+}) {
+    const [isPending,startTransition]=useTransition()
   return (
     <main className=' flex flex-col items-center space-y-1'>
         <H1>PetSoft lifetime access </H1>
-        <Button
-        onClick={async()=>{
-            await createCheckoutSession()
-        }}>Buy @ $1</Button>
+        {
+            !searchParams.success && (
+            
+            <Button 
+            disabled={isPending}
+            onClick={async()=>{
+                startTransition(
+                    async()=>{
+
+                        await createCheckoutSession()
+                    }
+                )
+            }}>Buy @ $1</Button>
+
+            )
+        }
+        {
+            searchParams.success &&(
+                <p className='text-sm text-green-700'>Payment succesful! Welcome to petsoft dream journey</p>
+            )
+        }
+        {searchParams.cancelled && (
+            <p className='text-sm text-red-700'>
+                Payment cancelled .Please try again...
+            </p>
+        )
+
+        }
     </main>
   )
 }
